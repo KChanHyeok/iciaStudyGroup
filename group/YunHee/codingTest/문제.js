29d8b66b84f35d77b8a04bdb823e21f6c01a2144
Yuni <apjm77@naver.com>
2022-09-01 17:28:52 +0900

    문제확인

--- group/YunHee/codingTest/문제.js
+++ group/YunHee/codingTest/문제.js
@@ -1,12 +1,13 @@
 
 arr = [1, 1, 3, 3, 0, 1, 1]
 
-function solution(arr) {
+function solution(arr)
+{
     var answer = [];
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     console.log('Hello Javascript')
-
+    
     return answer;
 }
 
