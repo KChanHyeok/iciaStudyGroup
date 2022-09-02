2bd977e63771692813b9efefcb077aff0e1ab632
KChanHyeok <kang9kang9@gmail.com>
2022-09-02 12:28:56 +0900

    완주하지못한선수

---
+++ group/kchan/CodingTest/완주하지 못한 선수/문제.js
@@ -0,0 +1,9 @@
+
+solution(["leo", "kiki", "eden"], ["eden", "kiki"])
+// 문2 solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"])    
+// 문3 solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+
+function solution(participant, completion) {
+    var answer = '';
+    return answer;
+}
