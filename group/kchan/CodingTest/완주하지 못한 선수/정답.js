324fec496f4d9f3940e9b1aa58020ffbc9d66f86
KChanHyeok <kang9kang9@gmail.com>
2022-09-02 13:07:02 +0900

    정답추가

--- group/kchan/CodingTest/완주하지 못한 선수/정답.js
+++ group/kchan/CodingTest/완주하지 못한 선수/정답.js
@@ -1,1 +1,11 @@
-// 제출먼저하세요....
+function solution(participant, completion) {
+    var answer = '';
+    participant.sort()
+    completion.sort()
+    for(let i=0; i<=participant.length; i++){
+        if(participant[i] !== completion[i]){
+            answer=participant[i]
+            return answer; 
+        }
+    }
+}
